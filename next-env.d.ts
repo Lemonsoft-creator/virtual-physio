01b2fd9dad56c9bc440fc2800282429d00008e45
Lemonsoft-creator <lemonsoft1990@gmail.com>
2025-04-16 17:55:09 +0200

    Update next-env.d.ts

--- next-env.d.ts
+++ next-env.d.ts
@@ -1,2 +1,7 @@
 "@types/node": "^20.11.0",
 "@types/react": "^18.2.40"
+/// <reference types="next" />
+/// <reference types="next/image-types/global" />
+
+// NOTE: This file should not be edited
+// see https://nextjs.org/docs/basic-features/typescript for more information.
