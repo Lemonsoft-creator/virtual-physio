c37c3d890d972b1ce3e22647a6675d84ee490f33
Lemonsoft-creator <lemonsoft1990@gmail.com>
2025-04-17 15:32:36 +0200

    Update head.tsx

--- app/head.tsx
+++ app/head.tsx
@@ -2,7 +2,8 @@ export default function Head() {
   return (
     <>
       <title>Virtueller Physio</title>
+      <meta name="description" content="Deine digitale Plattform für moderne Physiotherapie" />
       <meta name="viewport" content="width=device-width, initial-scale=1" />
     </>
-  )
+  );
 }
