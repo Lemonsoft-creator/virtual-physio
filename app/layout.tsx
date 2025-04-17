8da28ac1365df11d31343082d6e712a736ed95cd
Lemonsoft-creator <lemonsoft1990@gmail.com>
2025-04-17 15:32:19 +0200

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,14 +1,10 @@
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode
-}) {
+import "./globals.css";
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="de">
       <head />
-      <body className="bg-gray-50 text-gray-900">
-        {children}
-      </body>
+      <body className="bg-gray-50">{children}</body>
     </html>
-  )
+  );
 }
